7cd3286f81600052fb8d86c9024d013a60d75751
Sarahshr <saraah.sahraoui@gmail.com>
2020-10-27 11:03:08 +0100

    work on Header.css, change title on index.html

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,9 +4,10 @@ import logo from '../../../public/logo.png';
 function Header() {
   return (
     <div className="background-header">
-      <p>Hello</p>
-      <img src={logo} className="logo" alt="Logo voltasic" />
-      <input placeholder="Search" />
+      <div className="bloc">
+        <img src={logo} className="logo" alt="Logo voltasic" />
+        <input placeholder="Search" className="search-bar" />
+      </div>
     </div>
   );
 }
