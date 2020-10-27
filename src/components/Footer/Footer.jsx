c94c7e0d865213875d339067f395fcc94efd27cf
Fx-Colliot <fxcolliot@gmail.com>
2020-10-27 13:46:06 +0100

    add footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,14 @@
+import './Footer.css';
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <a href="CGU">CGU</a>
+
+      <a href="Contact">Contact</a>
+      <a href="Parametres">Parametres</a>
+    </div>
+  );
+};
+
+export default Footer;
