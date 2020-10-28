5aa045b1ef96dcff1a654f0e3143c1039d9d7356
Fx-Colliot <fxcolliot@gmail.com>
2020-10-28 10:42:22 +0100

    footer responsive avec icon en mobil

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,10 +3,23 @@ import './Footer.css';
 const Footer = () => {
   return (
     <div className="footer">
-      <a href="CGU">CGU</a>
+      <div className="footer-desk">
+        <a href="CGU">CGU</a>
 
-      <a href="Contact">Contact</a>
-      <a href="Parametres">Parametres</a>
+        <a href="Contact">Contact</a>
+        <a href="Parametres">Parametres</a>
+      </div>
+      <div className="footer-responsive">
+        <a href="homme">
+          <i className="fas fa-home" />
+        </a>
+        <a href="contact">
+          <i className="fas fa-at" />
+        </a>
+        <a href="settings">
+          <i className="fas fa-cog" />
+        </a>
+      </div>
     </div>
   );
 };
